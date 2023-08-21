49e2b20f35530ef5408be67cdc54d5226d91a94b
admin <tmdco12@naver.com>
2023-08-21 16:00:53 +0900

    DB와 연결을 하지 않았기 때문에 메모 데이터를 저장할 Java 컬렉션 생성

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -2,14 +2,20 @@ package com.sparta.memo.controller;
 
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
