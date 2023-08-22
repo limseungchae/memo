4863d7ce070620022dd7043e82c1b147d5c59a0e
admin <tmdco12@naver.com>
2023-08-22 20:26:37 +0900

    Controller 설명문 추가

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -11,47 +11,58 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// 'RestController' 어노테이션: 이 클래스가 REST API 엔드포인트를 처리하는 컨트롤러임을 나타냄
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api") // 엔드포인트의 기본 경로 설정
 public class MemoController {
 
-    private final Map<Long, Memo> memoList = new HashMap<>();
+    private final Map<Long, Memo> memoList = new HashMap<>(); // 메모를 저장하기 위한 Map 생성
 
+    // HTTP POST 요청 처리
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
         // RequestDto -> Entity
+        // 새 메모 생성
         Memo memo = new Memo(requestDto);
 
         // Memo Max ID Check
+        // 메모 ID 설정
         Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
         memo.setId(maxId);
 
         // DB 저장
+        // 메모를 메모 목록에 추가
         memoList.put(memo.getId(), memo);
 
         // Entity -> ResponseDto
+        // 생성된 메모 정보를 응답
         MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
 
         return memoResponseDto;
     }
 
+    // HTTP GET 요청 처리
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
         // Map To List
+        // 메모 목록을 응답용 DTO 리스트로 변환
         List<MemoResponseDto> responseList = memoList.values().stream()
                 .map(MemoResponseDto::new).toList();
 
         return responseList;
     }
 
+    // HTTP PUT 요청 처리
     @PutMapping("/memos/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
         // 해당 메모가 DB에 존재하는지 확인
+        // 주어진 ID로 메모를 찾음
         if(memoList.containsKey(id)) {
             // 해당 메모 가져오기
             Memo memo = memoList.get(id);
 
             // memo 수정
+            // 메모 내용 업데이트
             memo.update(requestDto);
             return memo.getId();
         } else {
@@ -59,11 +70,14 @@ public class MemoController {
         }
     }
 
+    // HTTP DELETE 요청 처리
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
         // 해당 메모가 DB에 존재하는지 확인
+        // 주어진 ID로 메모를 찾음
         if(memoList.containsKey(id)) {
             // 해당 메모 삭제하기
+            // 메모 삭제
             memoList.remove(id);
             return id;
         } else {
